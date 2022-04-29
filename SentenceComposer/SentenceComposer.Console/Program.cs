4933faa54de0b4c4c17128a2e3f07431208e3b6e
Vadim <nikifovadim@gmail.com>
2022-04-29 14:16:19 +0300

    Sentence composer added namespaces

--- SentenceComposer/SentenceComposer.Console/Program.cs
+++ SentenceComposer/SentenceComposer.Console/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System.Threading.Tasks;
 using SentenceComposer.Business.Services.Implementations;
 using SentenceComposer.Business.Services.Interfaces;
 
